2f1db214b40d51e78760579ee22396b2d847af32
unknown <tecmatheusg@hotmail.com>
2023-07-16 20:40:28 -0300

    Alteração em uma das imagens de projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-import Navbar from './components/Navbar';
 
 import ProjetosDiv from './components/ProjetosDiv';
 import ExperienciaDiv from './components/ExperienciaDiv';
