0019b9f375819e9bafa96142fc5c9af4a9980d93
matheus.silva <tecmatheusg@hotmail.com>
2023-08-09 13:43:01 -0300

    Alterações na seção Contatos

--- src/components/ContatosDiv/index.js
+++ src/components/ContatosDiv/index.js
@@ -3,33 +3,34 @@ import Titulo from "../Titulo";
 import 'boxicons'
 
 export default function ContatosDiv() {
-    const buttonSize = '70px';
+    const buttonSize = '65px';
     const [elementOpacity, setElementOpacity] = useState(0);
+    const [elementTransition, setElementTransition] = useState('opacity 2s ease, transform 1s ease');
 
     const handleScroll = () => {
         const divElement = document.getElementById('contato');
         if (divElement) {
-          const divPosition = divElement.getBoundingClientRect();
-          const windowHeight = window.innerHeight;
-          
-          if (divPosition.top <= windowHeight - 300) {
-            console.log('Aqui quero que o elemento apareça');
-            setElementOpacity(1); 
-          }
-          else{
-            setElementOpacity(0);
-            console.log('Aqui quero que o elemento suma');
-          }
+            const divPosition = divElement.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+
+            if (divPosition.top <= windowHeight - 300) {
+                setElementOpacity(1);
+                setElementTransition('opacity 2s ease, transform 1s ease');
+            }
+            else {
+                setElementTransition('none');
+                setElementOpacity(0);
+            }
         }
-      };
-    
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-    
+
         return () => {
-          window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll);
         };
-      }, []);
+    }, []);
 
 
     return (
@@ -37,8 +38,14 @@ export default function ContatosDiv() {
             <div className='min-vh-100 text-white pb-5 d-flex flex-column' id="contato">
                 <Titulo titulo="Contatos" />
                 <div className="mx-auto my-auto">
-                    <a target='_blank' className='item-contato' style={{opacity: elementOpacity}} href='https://www.linkedin.com/in/matheusgs1/'><box-icon size={buttonSize} type='logo' name='linkedin-square' color='#FFFFFF'></box-icon></a>
-                    <a target='_blank' className='item-contato' style={{opacity: elementOpacity}} href='https://github.com/matheusg1'><box-icon size={buttonSize} type='logo' name='github' color='#FFFFFF'></box-icon></a>
+                    <a target='_blank' className="px-1" style={{
+                        opacity: elementOpacity,
+                        transition: elementTransition
+                    }} href='https://www.linkedin.com/in/matheusgs1/'><box-icon size={buttonSize} type='logo' name='linkedin-square' color='#FFFFFF'></box-icon></a>
+                    <a target='_blank' className="px-1" style={{
+                        opacity: elementOpacity,
+                        transition: elementTransition
+                    }} href='https://github.com/matheusg1'><box-icon size={buttonSize} type='logo' name='github' color='#FFFFFF'></box-icon></a>
                 </div>
             </div>
         </>
